c6094e9136a9002864cad77bb44a43bc92345acf
Gerald McAlister <GEMISIS@users.noreply.github.com>
2026-09-17 21:55:54 -0700

    chore: bump beta manifests to 0.6.2-beta (#68)
    * chore: bump beta leviath-beta.rb to 0.6.2-beta
    
    * chore: bump beta leviath-beta.json to 0.6.2-beta
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- Formula/leviath-beta.rb
+++ Formula/leviath-beta.rb
@@ -8,7 +8,7 @@ class LeviathBeta < Formula
   desc "A structured agent runtime for LLMs (weekly beta)"
   homepage "https://leviath.dev"
   license "MIT"
-  version "0.5.9-beta"
+  version "0.6.2-beta"
 
   on_macos do
     if Hardware::CPU.arm?
